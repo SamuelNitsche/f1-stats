986170a8ae91d303ba6559a563507768d4843160
Samuel Nitsche <mail@samynitsche.de>
2021-10-23 00:26:30 +0200

    Add ability to fetch drivers per season

---
+++ database/migrations/2021_10_22_214036_create_driver_season_table.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('driver_season', function (Blueprint $table) {
+            $table->foreignId('driver_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('season_id')->constrained()->cascadeOnDelete();
+        });
+    }
+};
