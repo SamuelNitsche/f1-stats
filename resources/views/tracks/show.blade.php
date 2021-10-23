252bee5158ffdacb6c943e39c7d45b953b755233
Samuel Nitsche <mail@samynitsche.de>
2021-10-23 19:54:41 +0200

    Display qualifying and race details per track

--- resources/views/tracks/show.blade.php
+++ resources/views/tracks/show.blade.php
@@ -1,21 +1,68 @@
 <p>{{ $track->name }}</p>
 
-<p>Fastest qualifying track overall: {{ \App\Models\DriverQualification::forTrack($track)->min('q1_time') }}</p>
+<details>
+  <summary>Seasons</summary>
+  @foreach($track->rounds as $round)
+    <details>
+      <summary>{{ $round->season->year }}</summary>
 
-<ul>
-  @foreach($track->qualifications as $qualification)
-    <li>
-      {{ $qualification->season->year }}
-      &mdash;
-      {{ $qualification->track->name }}
-{{--      &mdash;--}}
-{{--      {{ $qualification->pivot->position }}--}}
-{{--      &mdash;--}}
-{{--      {{ $qualification->pivot->q1_time }}--}}
-{{--      &mdash;--}}
-{{--      {{ $qualification->pivot->q2_time ?? 'N/A' }}--}}
-{{--      &mdash;--}}
-{{--      {{ $qualification->pivot->q3_time ?? 'N/A' }}--}}
-{{--    </li>--}}
+      <details>
+        <summary>Qualifying</summary>
+
+        <table style="text-align: left">
+          <thead>
+          <tr>
+            <th>Position</th>
+            <th>Driver Name</th>
+            <th>Q1 Time</th>
+            <th>Q2 Time</th>
+            <th>Q3 Time</th>
+          </tr>
+          </thead>
+          <tbody>
+          @foreach($round->qualification->drivers as $driver)
+            <tr>
+              <td>{{ $driver->pivot->position }}</td>
+              <td>{{ $driver->full_name }}</td>
+              <td>{{ $driver->pivot->q1_time }}</td>
+              <td>{{ $driver->pivot->q2_time ?? 'N/A' }}</td>
+              <td>{{ $driver->pivot->q3_time ?? 'N/A' }}</td>
+            </tr>
+          @endforeach
+          </tbody>
+        </table>
+      </details>
+
+      <details>
+        <summary>Race</summary>
+
+        <table style="text-align: left;">
+          <thead>
+          <tr>
+            <th>Position</th>
+            <th>Driver Name</th>
+            <th>Grid</th>
+            <th>Total Time</th>
+            <th>Status</th>
+            <th>Laps</th>
+            <th>Points</th>
+          </tr>
+          </thead>
+          <tbody>
+          @foreach($round->race->drivers as $driver)
+            <tr>
+              <td>{{ $driver->pivot->position }}</td>
+              <td>{{ $driver->full_name }}</td>
+              <td>{{ $driver->pivot->grid }}</td>
+              <td>{{ $driver->pivot->total_time }}</td>
+              <td>{{ $driver->pivot->status }}</td>
+              <td>{{ $driver->pivot->laps }}</td>
+              <td>{{ $driver->pivot->points }}</td>
+            </tr>
+          @endforeach
+          </tbody>
+        </table>
+      </details>
+    </details>
   @endforeach
-</ul>
+</details>
