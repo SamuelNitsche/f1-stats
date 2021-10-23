ed707304cc926079839e3c90ccf34cbc9bb2ba7a
Samuel Nitsche <mail@samynitsche.de>
2021-10-23 17:22:19 +0200

    Show qualifications per driver

--- resources/views/drivers/show.blade.php
+++ resources/views/drivers/show.blade.php
@@ -5,3 +5,21 @@
     <li>{{ $season->year }}</li>
   @endforeach
 </ul>
+
+<ul>
+    @foreach($driver->qualifications as $qualification)
+        <li>
+          {{ $qualification->season->year }}
+          &mdash;
+          {{ $qualification->track->name }}
+          &mdash;
+          {{ $qualification->pivot->position }}
+          &mdash;
+          {{ $qualification->pivot->q1_time }}
+          &mdash;
+          {{ $qualification->pivot->q2_time ?? 'N/A' }}
+          &mdash;
+          {{ $qualification->pivot->q3_time ?? 'N/A' }}
+        </li>
+    @endforeach
+</ul>
