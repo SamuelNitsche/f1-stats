64d64ad52f42788a226040298e7cb050676e580b
Samuel Nitsche <mail@samynitsche.de>
2021-11-23 11:03:31 +0100

    Cache season standings

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Season extends Model
 {
@@ -26,12 +27,14 @@ class Season extends Model
 
     public function getStandings()
     {
-        return $this
-            ->drivers()
-            ->with('races')
-            ->get()
-            ->sortByDesc(function (Driver $driver) {
-                return $driver->points($this);
-            });
+        return Cache::remember("standings-{$this->year}", now()->addMinutes(10), function () {
+            return $this
+                ->drivers()
+                ->with('races')
+                ->get()
+                ->sortByDesc(function (Driver $driver) {
+                    return $driver->points($this);
+                });
+        });
     }
 }
