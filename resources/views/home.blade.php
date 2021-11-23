9b4c7723cab0fe643cf1732f341e1c003a4a8974
Samuel Nitsche <mail@samynitsche.de>
2021-11-23 11:03:50 +0100

    Display current standings on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,41 @@
 @section('title', 'Home')
 
 @section('content')
-  <p>Last Race</p>
-  <p>
-    <a
-        href="{{ route('rounds.show', [$previousRound->season, $previousRound]) }}"
-    >{{ $previousRound->name }}</a>
-  </p>
+    <p>Last Race</p>
+    <p>
+        <a
+            href="{{ route('rounds.show', [$previousRound->season, $previousRound]) }}"
+        >{{ $previousRound->name }}</a>
+    </p>
 
-  <br>
+    <br>
 
-  <p>Next Race</p>
-  <p>
-    <a
-        href="{{ route('rounds.show', [$upcomingRound->season, $upcomingRound]) }}"
-    >{{ $upcomingRound->name }} - in <time datetime="{{ $upcomingRound->date }}">{{ $upcomingRound->date->longAbsoluteDiffForHumans() }}</time></a>
-  </p>
+    <p>Next Race</p>
+    <p>
+        <a
+            href="{{ route('rounds.show', [$upcomingRound->season, $upcomingRound]) }}"
+        >{{ $upcomingRound->name }} - in
+            <time datetime="{{ $upcomingRound->date }}">{{ $upcomingRound->date->longAbsoluteDiffForHumans() }}</time>
+        </a>
+    </p>
+
+    <p>Current standins</p>
+    <table class="text-left">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Points</th>
+        </tr>
+        </thead>
+        <tbody></tbody>
+        @foreach ($currentSeason->getStandings() as $driver)
+            <tr>
+                <td>{{ $loop->index + 1 }}</td>
+                <td>{{ $driver->full_name }}</td>
+                <td>{{ $driver->points($currentSeason) }}</td>
+            </tr>
+        @endforeach
+    </table>
+
 @endsection
