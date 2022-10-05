fd50494bed153899e64b1d3f9dc835805056849e
Samuel Nitsche <mail@samynitsche.de>
2022-10-05 23:42:18 +0200

    Added test for filterable letter in name

--- tests/Feature/Http/DriverOverviewPageTest.php
+++ tests/Feature/Http/DriverOverviewPageTest.php
@@ -10,3 +10,14 @@ it('displays all drivers by first letter of surname', function () {
     \Pest\Laravel\get('/drivers')
         ->assertSee($names);
 });
+
+it('letter can be filtered in url', function () {
+    $names = \App\Models\Driver::query()
+        ->where('surname', 'like', 'F%')
+        ->get(['forename', 'surname'])
+        ->map(fn ($driver) => "{$driver->forename} {$driver->surname}")
+        ->toArray();
+
+    \Pest\Laravel\get('/drivers?letter=F')
+        ->assertSee($names);
+});
